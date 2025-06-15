9e1aebd0c97b48c1d79bb263a884c7e21e62a9e1
Youssef Safwat <youssef.safwat.dev@gmail.com>
2025-06-15 14:41:14 +0300

    Add error handling middleware

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,39 @@
+const errorMiddleware = (err, req, res, next) => {
+  try {
+    let error = { ...err };
+    error.message = err.message;
+    console.log(err);
+
+    // mongoose bad objectId
+
+    if (err.name === 'CastError') {
+      const message = 'Resource not found';
+      error = new Error(message);
+      error.statusCode = 404;
+    }
+
+    // Mongoose duplicate key
+
+    if (err.code === 11000) {
+      const message = 'Duplicate field value entered';
+      error = new Error(message);
+      error.statusCode = 400;
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map((val) => val.message);
+
+      error = new Error(message.join(', '));
+      error.statusCode = 404;
+    }
+
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message || 'Server Error' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
